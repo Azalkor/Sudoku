64e97f6a8182cdf4640609b29644d7f46661b25a
thomas <thomas@debian>
2015-12-05 17:25:56 +0100

    traitement de la valeur = sur ligne et colonne mais pas dans le carré

--- sudoku.lisp
+++ sudoku.lisp
@@ -13,6 +13,19 @@
       (0 8 0 0 1 6 0 0 0)
       (5 0 0 2 0 0 0 0 7)))
 
+;;init grille
+;(defparameter *grid*
+;  #2A((1 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;     (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)
+;      (0 0 0 0 0 0 0 0 0)))
+
+
 ;;fonction qui nous affiche la grille
 (defun sudoku() 
   (format t "   | A B C | D E F | G H I |~C" #\linefeed)
@@ -29,23 +42,72 @@
 	    (if (zerop nb)
 		(format t "  ")
 		(format t "~D " nb)))))
-      (if (zerop (mod (+ l 1) SIZE))
-	  (progn 
-	    (format t "~C" #\linefeed)
-	    (dotimes (tmp (+ (* AREA SIZE) 1))
-	      (format t "-")))
-	  NIL)))
+    (if (zerop (mod (+ l 1) SIZE))
+	(progn 
+	  (format t "~C" #\linefeed)
+	  (dotimes (tmp (+ (* AREA SIZE) 1))
+	    (format t "-")))
+	NIL)))
 
+;;On demande au joueur de choisir la ligne la colonne et la valeur d'un case
 (defun user-read ()
   (let ((l (progn (princ "ligne (entier) ? ") (read)))
 	(c (progn (princ "colonne (lettre) ? ") (read)))
 	(val (progn (princ "valeur ? ") (read))))
-    (if (not (is-correct (- l 1) (- c 1) val))
-	(user-read)
+    (if (not-correct (- l 1) (- c 1) val)
+	(progn 
+	  (format t "valeur existante ligne et/ou colonne et/ou 3x3~C" #\linefeed)
+	  (user-read))
 	(setf (aref *grid* (- l 1) (- c 1)) val))))
 
-(defun is-correct (l c val)
-  (if (or (or (>= 0 val) (> val AREA)) (or (> 0 l) (> l (- AREA 1))) (or (> 0 c) (> c (- AREA 1))))
-      NIL
-      T))
-      
+;;On vérifie si la valeur, la ligne et la colonne sont correct
+(defun not-correct (l c val)
+  (if (or (>= 0 val) (> val AREA) (> 0 l) (> l (- AREA 1)) (> 0 c) (> c (- AREA 1)))
+      T
+      (progn
+	(dotimes (tmpLC AREA)
+	  (cond ((or (= (aref *grid* tmpLC c) val) (= (aref *grid* l tmpLC) val))
+		 (return-from exit))
+		))
+	(dotimes (tmpL SIZE)
+	  (dotimes (tmpC SIZE)
+	    (cond ((< l SIZE)
+		   (cond ((and (< c SIZE) (>= c 0) (= (aref *grid* tmpL tmpC) val))
+			  (format t " yolo tmpl~D tmpc~D ~C" tmpL tmpC #\linefeed)
+			  (return T))
+			 ((and (< c (* SIZE 2)) (>= c SIZE) (= (aref *grid* tmpL (+ tmpC SIZE)) val))
+			  (format t " yolo tmpl~D tmpc~D ~C" tmpL (+ tmpC SIZE) #\linefeed)
+			  (return T))
+			 ((and (< c AREA) (>= c (* SIZE 2)) (= (aref *grid* tmpL (+ tmpC (* SIZE 2))) val))
+			  (format t " yolo tmpl~D tmpc~D ~C" tmpL (+ tmpC (* SIZE 2)) #\linefeed)
+			  (return T)))))))))))
+
+;	(dotimes (tmpL SIZE)
+;	  (dotimes (tmpC SIZE)
+;	    (if (< l SIZE)
+;		(if (< c SIZE)
+;		    (if (= (aref *grid* tmpL tmpC) val)
+;			(return T)
+;;			NIL)
+;		    (if (< c (* SIZE 2)) 
+;			(if (= (aref *grid* tmpL (+ tmpC SIZE)) val)
+;			    (progn
+;			      (format t " yolo tmpl~D tmpc~D ~C" tmpL (+ tmpC SIZE) #\linefeed)
+;			      (return T))
+;;			    NIL)
+;			(if (< c AREA)
+;			    (if (= (aref *grid* tmpL (+ tmpC (* SIZE 2))) val)
+;				(return T)
+;;				NIL)
+;			    )
+;			)
+;		    )
+;		NIL))))))
+;
+
+
+(defun main ()
+  (sudoku)
+  (format t "~C" #\linefeed)
+  (user-read)
+  (main))
